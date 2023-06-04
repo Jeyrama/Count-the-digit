8b53912b04649f06f55bb8650966eb443f8989de
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-06-04 12:36:12 -0700

    Update Count-the-digit.js

--- Count-the-digit.js
+++ Count-the-digit.js
@@ -31,3 +31,10 @@ function nbDig(n, d) {
 
 // or
 
+function nbDig(n, d) {
+  let totalOfDigit = 0;
+  for(let i = 0; i <= n; i++) {
+    totalOfDigit += (i*i).toString().split('').filter(n => n == d).length;
+  }
+  return totalOfDigit;
+}
